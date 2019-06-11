3d1adae14cd2f46b4d87d9a147a4aa9356acc94b
Henrique Diniz <henriquee.diniz@hotmail.com>
2019-06-10 22:29:38 -0300

    test folder with tests

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+
+const app = require("../../src/app");
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+  it("should authenticate with valid credentials", async () => {
+    const user = await User.create({
+      name: "Henrique",
+      email: "henriquee.diniz@hotmail.com",
+      password_hash: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.status).toBe(200);
+  });
+});
